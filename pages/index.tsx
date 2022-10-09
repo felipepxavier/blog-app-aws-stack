114f8e08b27a483ba891b5d3981c39d6228e48a6
Felipe Pedroso Xavier <felipe.sadler@gmail.com>
2022-10-09 15:12:18 -0300

    feat: list posts with graphql

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,39 @@
+import { ListPostsQuery, Post } from '../src/API'
+import {useEffect, useState} from 'react'
+
+import { API } from 'aws-amplify'
 import type { NextPage } from 'next'
+import { listPosts } from '../src/graphql/queries'
 
 const Home: NextPage = () => {
+  const [posts, setPosts] = useState<Array<Post | null>>([])
+
+  useEffect(() => {
+    async function fetchPosts() {
+      const postData = (await API.graphql({
+        query: listPosts
+      })) as { data: ListPostsQuery }
+      
+      if (postData.data.listPosts?.items) {
+        setPosts(postData.data.listPosts.items)
+      }
+    }
+
+    fetchPosts()
+  }, [])
+
   return (
-    <h1 className="text-6xl font-bold underline text-sky-400">
-      Hello world!
+   <div>
+     <h1 className="text-6xl font-bold underline text-sky-400">
+      Minhas Postagens
     </h1>
+
+    {
+      posts.map((post) => 
+      (<p key={post?.id}>{post?.title}</p>)
+      )
+    }
+   </div>
   )
 }
 
